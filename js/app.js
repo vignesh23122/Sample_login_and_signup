b4b20aa0c072d9a1af98242cf1b788056df9e3cb
vignesh2312 <srivignesh23122001@gmail.com>
2023-07-16 19:35:06 +0530

    intial commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+//     $("#signup").click(function () {
+//         var fname=$("#fname").val();
+//         var lname=$("#lname").val();
+//         var email=$("#mail").val();
+//         var password=$("#pass").val();
+//         var data="user=" + fname+"&pass"+password;
+//         $.ajax({
+//             url:"app.php?",
+//             method:"post",
+//             data:data,
+//             success:function(data){
+//                 $("#cl").html(data);
+//             }
+//         });
+
+//     });
+// });
+// function click() {
+//     alert("hello");
+// }
